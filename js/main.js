3d5d29255778618f75f6218aa045a37147e1336f
Steven Yee <s.yee615@gmail.com>
2020-05-06 14:44:58 -0400

    Updated Details

--- js/main.js
+++ js/main.js
@@ -70,12 +70,7 @@ $( 'pre' ).text( $( 'pre' ).html() );
 
 var modal        = document.getElementById('modal'),
     modalContent = document.querySelector('.modal-content'),
-    openModal    = document.querySelector('.js-open-modal'),
     closeModal   = document.querySelectorAll('.modal, .modal-close');
-
-openModal.addEventListener('click', function() {
-  modal.classList.add('is-visible');
-});
 
 [].forEach.call(closeModal, function ( el ) {
    el.addEventListener('click', function() {
